f65eb51bd9b3cbd4ff542262aeb5b1c0a7345e79
Indra W <indra.roadota@gmail.com>
2022-03-17 08:07:54 +0700

    router+list

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./Main"
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Navbar />
+                <Main />
+            </div>
+        )
+    }
+}
+export default App;
+
